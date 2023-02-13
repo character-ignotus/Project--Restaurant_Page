1295b7e284871f266ac4aa2731701fbd9094ff8a
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-02-13 17:05:57 +0200

    Auto generate dist folder with an index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -9,4 +10,10 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      filename: 'index.html',
+    })
+  ]
 };
