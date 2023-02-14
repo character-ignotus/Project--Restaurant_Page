6890a503d9194f83cd9f694658cef4a7b59728bb
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-02-14 22:20:10 +0200

    Apply second styling to home page

--- src/home.js
+++ src/home.js
@@ -3,9 +3,21 @@ export default function homePageLoader() {
     const imgSection = document.createElement('div');
     const textSection = document.createElement('div');
     const text = document.createElement('div');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+    const p3 = document.createElement('p');
     const button = document.createElement('button');
     
+    p1.textContent = 'Come.';
+    p2.textContent = 'Sit.';
+    p3.textContent = 'Dine.';
+    button.textContent = 'Book A Table';
+
     element.classList.add('home-page');
+
+    text.appendChild(p1);
+    text.appendChild(p2);
+    text.appendChild(p3);
 
     textSection.appendChild(text);
     textSection.appendChild(button);
