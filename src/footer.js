2dba42eb0b363e371b4c428bb4d49e952120edba
Lyubomir Lyubomirov <l.lubomirov00@gmail.com>
2023-02-14 23:50:23 +0200

    Add styling to footer

--- src/footer.js
+++ src/footer.js
@@ -1,9 +1,12 @@
 export default function footerLoader() {
     const footer = document.createElement('footer');
     const paragraph = document.createElement('p');
-    paragraph.textContent = 'Footer text content';
-    
+    const img = document.createElement('div');
+
+    paragraph.textContent = 'Copyright © 2023 character-ignotus';
+
     footer.appendChild(paragraph);
+    footer.appendChild(img)
 
     return footer
 }
